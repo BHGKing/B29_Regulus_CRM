7e63b2e59cacc6de6ddaf036fb74eeecbe5a93af
maiyada <maiyada.hasan12@gmail.com>
2023-07-18 18:33:42 -0500

    Maiyada_US186 updated

--- src/test/java/com/cydeoCRM/runners/CukesRunner.java
+++ src/test/java/com/cydeoCRM/runners/CukesRunner.java
@@ -9,8 +9,8 @@ import org.junit.runner.RunWith;
         plugin = {
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
-                "json:target/cucumber.json"
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+
 
         },
         features = "src/test/resources/features",
