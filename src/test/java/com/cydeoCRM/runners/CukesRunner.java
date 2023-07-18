b6a6d680d8479d32d4fd01ecf432639a18c7a392
maiyada <maiyada.hasan12@gmail.com>
2023-07-18 13:54:44 -0500

    Maiyada_US186 done

--- src/test/java/com/cydeoCRM/runners/CukesRunner.java
+++ src/test/java/com/cydeoCRM/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeoCRM/step_definitions",
         dryRun = false,
-        tags = "@B29G44-201",
+        tags = "",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
