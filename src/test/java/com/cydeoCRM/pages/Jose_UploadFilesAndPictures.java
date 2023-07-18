52a9f258d8e4bc77e5498694c8a4c557fd610351
BHGKing <josecantu111@gmail.com>
2023-07-18 18:44:11 -0500

    Update project

--- src/test/java/com/cydeoCRM/pages/Jose_UploadFilesAndPictures.java
+++ src/test/java/com/cydeoCRM/pages/Jose_UploadFilesAndPictures.java
@@ -1,4 +1,14 @@
 package com.cydeoCRM.pages;
 
+import com.cydeoCRM.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 public class Jose_UploadFilesAndPictures {
+
+    public Jose_UploadFilesAndPictures(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
 }
