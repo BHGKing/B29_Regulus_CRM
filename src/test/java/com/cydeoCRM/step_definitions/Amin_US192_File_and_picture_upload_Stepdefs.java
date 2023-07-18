2ca2298a08c1a41947940e4eb1d86434279d3d66
AminulhaqMomad <Amin@gmail.com>
2023-07-18 17:51:16 -0500

    File_and_picture stepDef file added

---
+++ src/test/java/com/cydeoCRM/step_definitions/Amin_US192_File_and_picture_upload_Stepdefs.java
@@ -0,0 +1,22 @@
+package com.cydeoCRM.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class Amin_US192_File_and_picture_upload_Stepdefs {
+    @Given("user is on the appreciation module of the home page")
+    public void userIsOnTheAppreciationModuleOfTheHomePage() {
+    }
+
+    @Then("user click on the files upload icon")
+    public void userClickOnTheFilesUploadIcon() {
+    }
+
+    @Then("user choose file from local directory")
+    public void userChooseFileFromLocalDirectory() {
+    }
+
+    @Then("user able to upload Supported file formats .pdf, .txt, .jpeg, .png, .docx")
+    public void userAbleToUploadSupportedFileFormatsPdfTxtJpegPngDocx() {
+    }
+}
