8226e19e24d547983b9e7ae95a011baa1e812125
Iqbal <iqbalm.cydeo@gmail.com>
2023-07-20 09:36:13 -0600

    iqbal RunnerJson added

--- src/test/java/com/cydeoCRM/runners/CukesRunner.java
+++ src/test/java/com/cydeoCRM/runners/CukesRunner.java
@@ -7,15 +7,17 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
+                "pretty",
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
                 "json:target/cucumber.json"
+
         },
         features = "src/test/resources/features",
         glue = "com/cydeoCRM/step_definitions",
         dryRun = false,
-        tags = "",
+        tags = "@B29G44-229",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
